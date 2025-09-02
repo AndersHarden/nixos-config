d711539bcf62ccbbc7b21a0d4dbc567c91cbb2bf
Anders Hardenborg <anders.hardenborg@gmail.com>
2025-09-02 08:29:59 +0200

    Fix config undefined error in extraSpecialArgs

--- flake.nix
+++ flake.nix
@@ -2,15 +2,6 @@
 {
   description = "Min familj av NixOS-maskiner";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org/"
-    ];
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -19,7 +10,6 @@
 
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
     nixosConfigurations = {
-      # Your existing configurations (laptop-intel, laptop-nvidia, etc.)
       laptop-intel = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
@@ -33,13 +23,12 @@
             home-manager.extraSpecialArgs = {
               hostName = "laptop-intel";
               inherit inputs;
+              systemEtc = config.environment.etc; # Move systemEtc here
             };
             home-manager.users.anders = import ./modules/home/anders.nix;
           }
         ];
       };
-
-      # Nvidia laptop
       laptop-nvidia = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
@@ -51,15 +40,14 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {
-              hostName = "laptop-nvidia"; # Direkt värde
+              hostName = "laptop-nvidia";
               inherit inputs;
+              systemEtc = config.environment.etc;
             };
             home-manager.users.anders = import ./modules/home/anders.nix;
           }
         ];
       };
-
-      # Mediaspelare
       mediaplayer = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
@@ -71,15 +59,14 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {
-              hostName = "mediaplayer"; # Direkt värde
+              hostName = "mediaplayer";
               inherit inputs;
+              systemEtc = config.environment.etc;
             };
             home-manager.users.anders = import ./modules/home/anders.nix;
           }
         ];
       };
-
-      # Workstation
       workstation = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
@@ -91,8 +78,9 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {
-              hostName = "workstation"; # Direkt värde
+              hostName = "workstation";
               inherit inputs;
+              systemEtc = config.environment.etc;
             };
             home-manager.users.anders = import ./modules/home/anders.nix;
           }
