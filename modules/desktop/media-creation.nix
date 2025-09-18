4532f8aee6eca6a85f8984943605234dc61d2937
Anders Hardenborg <anders.hardenborg@gmail.com>
2025-09-18 13:18:01 +0200

    feat: add atomicparsley to media creation packages and nvidia specific packages

--- modules/desktop/media-creation.nix
+++ modules/desktop/media-creation.nix
@@ -26,5 +26,6 @@
 
     # Ljud
     openai-whisper
+    atomicparsley
   ];
 }
--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -7,5 +7,7 @@
   # nvidia specific packages
   environment.systemPackages = with pkgs; [ 
     cudaPackages.cudatoolkit
+    cudaPackages.cudnn
+    cudaPackages.libcublas
   ];
 }
