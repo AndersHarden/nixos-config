b3fed1f395e718ca280cb573b7360c86e4d9a6dc
Anders Hardenborg <anders.hardenborg@gmail.com>
2025-09-29 13:02:23 +0200

    fix: correct syntax for Python package imports in desktop configuration

--- modules/desktop/python.nix
+++ modules/desktop/python.nix
@@ -2,12 +2,12 @@
 {
   environment.systemPackages = with pkgs; [
     python313
-    (with python313Packages; [
-        pip
-        numpy
-        librosa
-        pydub
-        openai-whisper
-      ])
-  ];
+  ] ++ (with python313Packages; [
+    pip
+    numpy
+    librosa
+    pydub
+    openai-whisper
+  ]);
+
 }
