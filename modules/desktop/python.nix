60b65747b576a1e23b882fa90ae6ee7c7b8a4f9a
Anders Hardenborg <anders.hardenborg@gmail.com>
2025-09-29 12:58:41 +0200

    refactor: restructure Python package imports for clarity

--- modules/desktop/python.nix
+++ modules/desktop/python.nix
@@ -2,11 +2,12 @@
 {
   environment.systemPackages = with pkgs; [
     python313
-    python313Packages.pip
-    python313Packages.numpy
-    python313Packages.librosa
-    python313Packages.pydub
-    python313Packages.openai-whisper
+    (with python313Packages; [
+        pip
+        numpy
+        librosa
+        pydub
+        openai-whisper
+      ])
   ];
-
 }
