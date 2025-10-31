67513e51d721b52c5cdfb1b06353fc0e9adbbf68
Anders Hardenborg <anders.hardenborg@gmail.com>
2025-10-31 20:27:14 +0100

    fix: add missing newline at end of media-creation.nix and update comment in python.nix

--- modules/desktop/python.nix
+++ modules/desktop/python.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    uv # Extremely fast Python package installer and resolver, written in Rust
     python313
   ] ++ (with python313Packages; [
     pip
